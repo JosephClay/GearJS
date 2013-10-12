71ca861439fff1f5e0173ecf2530d977ba5f18bd
Joseph Clay <testingriven@gmail.com>
2013-10-12 10:31:29 -0500

    Class that throttles fps defined ticks

---
+++ src/utils/Framerate.js
@@ -0,0 +1,66 @@
+/**
+ * An easy way to subscribe to a tick with
+ * a throttle framerate.
+ *
+ * Inspired from:
+ * http://codetheory.in/controlling-the-frame-rate-with-requestanimationframe/
+ * Main comment preserved in tick subscription
+ */
+(function(Gear) {
+
+	var Framerate = function(fps) {
+		/**
+		 * Our special Date.now() for tracking
+		 * when a frame should fire. See tick
+		 * subscription for more details
+		 * @type {Number} ms
+		 */
+		this.then = null;
+		this.tickId = null;
+		this.setFPS(fps);
+	};
+
+	Framerate.prototype = {
+
+	    on: function(name, callback) {
+			this.then = _.now();
+
+			this.tickId = Gear.Tick.subscribe('tick', function(e) {
+
+				var delta = e.now - then;
+				if (delta > this.interval) {
+			        // update time stuffs
+			         
+			        // Just `then = now` is not enough.
+			        // Lets say we set fps at 10 which means
+			        // each frame must take 100ms
+			        // Now frame executes in 16ms (60fps) so
+			        // the loop iterates 7 times (16*7 = 112ms) until
+			        // delta > interval === true
+			        // Eventually this lowers down the FPS as
+			        // 112*10 = 1120ms (NOT 1000ms).
+			        // So we have to get rid of that extra 12ms
+			        // by subtracting delta (112) % interval (100).
+			        // Hope that makes sense.
+			         
+			        then = now - (delta % this.interval);
+
+			        callback.call();
+		        }
+			});
+	    },
+
+	    off: function(name) {
+			if (!this.tickId) { return; }
+			Gear.Tick.unsubscribe(this.tickId);
+			this.tickId = null;
+	    },
+
+	    setFPS: function(fps) {
+			this.interval = (1000 / fps);
+	    }
+	};
+
+	Gear.Framerate = Framerate;
+
+}(Gear));
