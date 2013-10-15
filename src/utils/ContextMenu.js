c37106fee61d2602a19375cd9693ab9e49cd24ac
Joseph Clay <testingriven@gmail.com>
2013-10-14 23:41:55 -0500

    Add a class to prevent context menus

---
+++ src/utils/ContextMenu.js
@@ -0,0 +1,21 @@
+(function(Gear) {
+
+	var _BODY = Gear.DOC.getElementsByTagName('body')[0],
+		_isPreventing = false;
+
+	Gear.ContextMenu = {
+		prevent: function() {
+			if (_isPreventing) { return; }
+
+			_BODY.setAttribute('oncontextmenu', 'return false;');
+			_isPreventing = true;
+		},
+		allow: function() {
+			if (!_isPreventing) { return; }
+
+			_BODY.removeAttribute('oncontextmenu');
+			_isPreventing = false;
+		}
+	};
+
+}(Gear));
