3d244330d71a409eba61eeb3e58c22f3dac840c2
Joseph Clay <testingriven@gmail.com>
2013-10-17 22:57:06 -0500

    Add framerate unit test file

---
+++ test/gear/events/framerate.js
@@ -0,0 +1,14 @@
+/*
+asyncTest('Framerate: tick', function() {
+	expect(1);
+	
+	var framerate = Gear.Framerate(20);
+
+	var tests = function() {
+		ok(true, '');
+		start();
+	};
+
+	setTimeout(tests, 60);
+});
+*/
