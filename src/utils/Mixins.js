62a0caf558f7a1c59236298f5d890f847981c69f
Joseph Clay <testingriven@gmail.com>
2013-10-19 23:29:10 -0500

    Resolve conflicts

--- src/utils/Mixins.js
+++ src/utils/Mixins.js
@@ -43,7 +43,10 @@
 
 		// faster Math.round using bitmath
 		// http://stackoverflow.com/questions/8483357/why-is-math-round-in-javascript-slower-than-a-custom-built-function
+		// Only slightly faster than ~~0.5.
+		// This abstraction allows for negative numbers
 		round: function(num) {
+			if (num < 0) { return Math.round(num); }
 			var remainder = num % 1;
 			return num - remainder + ((((remainder / 1) + 1.5) >> 1) * 1);
 		},
